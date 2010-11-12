fa3f1484d62f1deb540444e2181b428b5093ca34
Ryan Rempel <rgrempel@gmail.com>
2010-11-12 06:20:17 -0600

    Add test for switching to less detailed objectclass.

--- test/spec/models/person_spec.rb
+++ test/spec/models/person_spec.rb
@@ -46,6 +46,17 @@ describe Person do
     p.get_ldap_hash[:mail].should == ["fredjones@gmail.com"]
   end
 
+  it "should be able to switch to a less detailed objectclass when updating" do
+    p = Person.new :first_name => "Fred", :last_name => "Jones", :email => "fredjones@gmail.com"
+    p.use_person_for_objectclass = false
+    p.save
+    p.ldap_exists?.should == true
+    p.get_ldap_hash[:mail].should == ["fredjones@gmail.com"]
+    p.use_person_for_objectclass = true
+    p.save
+    p.get_ldap_hash[:mail].should == []
+  end
+
   it "should not throw and error if deleting an ldap entry that does not exist" do
     p = Person.create :first_name => "Fred", :last_name => "Jones"
     p.ldap_exists?.should == true
